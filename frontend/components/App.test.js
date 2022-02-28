7bf35c3746a0bd6084c8b4e51a949b23ef597aa5
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-02-28 13:10:09 -0600

    wrote some tests, axios still not working

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,19 +1,18 @@
 // Write your tests here
-import React from 'react'; 
-import { render, screen, fireEvent } from '@testing-library/react'; 
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
-import AppClass from './AppClass'; 
-import AppFunctional from './AppFunctional';
+import AppClass from "./AppClass";
+import AppFunctional from "./AppFunctional";
 
+test("sanity", () => {
+  expect(true).toBe(true);
+});
 
-test('sanity', () => {
-  expect(true).toBe(false)
-})
- 
-test('AppClass Component Renders Without Error', () => {
-  render(<AppClass />)
-})
+test("AppClass Component Renders Without Error", () => {
+  render(<AppClass className="class-based" />);
+});
 
-test('FunctionalClass Component Renders Without Error', () => {
-  render(<AppFunctional />)
-})
+test("FunctionalClass Component Renders Without Error", () => {
+  render(<AppFunctional className="functional" />);
+});
