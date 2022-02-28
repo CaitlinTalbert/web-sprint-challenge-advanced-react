66467203d4282ea791ed5172ad5715e182b7b337
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-02-28 11:12:18 -0600

    I CAN MOVE THE SQUARE UP AND LEFT!!!!!!

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 export default class AppClass extends React.Component {
   state = {
     x: 2,
     y: 2,
     steps: 0,
-    email: '', 
-    message: ''
-  }
+    email: "",
+    message: "",
+  };
 
   /**
    *(1, 1) (2, 1) (3, 1)
@@ -17,60 +17,66 @@ export default class AppClass extends React.Component {
    */
 
   leftClick = () => {
-    console.log("click")
-  }
+    console.log("click");
+  };
 
   upClick = () => {
-    console.log("click")
-  }
+    console.log("click");
+  };
 
   rightClick = () => {
-    console.log("click")
-  }
+    console.log("click");
+  };
 
   downClick = () => {
-    console.log("click")
-  }
+    console.log("click");
+  };
 
   resetClick = () => {
-    console.log("click")
-  }
+    console.log("click");
+  };
 
-
-onChange = event => {
-  this.setState({ 
-    ...this.state, 
-    email: event.target.value
-  })
-}
-
-onSubmit = event => {
-  event.preventDefault(); 
-
-  axios.post('http://localhost:9000/api/result', this.state)
-  .then(resp => {
+  onChange = (event) => {
     this.setState({
-      message: resp.data.message
-    })
-  })
-  .catch(err => {
-    console.log({err})
-    if (this.state.email === 'foo@bar.baz') {
-      this.setState({ 
-        message: 'foo@bar.baz forbidden error'
-      })
-    } else if (this.state.email === '') {
-      this.setState({ 
-        ...this.state, 
-        message: 'Must be a valid email'
-      })
-    }
-  })
-}
+      ...this.state,
+      email: event.target.value,
+    });
+  };
 
+  onSubmit = (event) => {
+    event.preventDefault();
+    const payload = {
+      x: this.state.grid.x,
+      y: this.state.grid.y,
+      steps: this.state.counter,
+      email: this.state.email,
+    };
+    axios
+      .post("http://localhost:9000/api/result", payload)
+      .then((resp) => {
+        this.setState({
+          ...this.state,
+          message: resp.data.message,
+        });
+        console.log(this.state);
+      })
+      .catch((err) => {
+        console.log({ err });
+        if (this.state.email === "foo@bar.baz") {
+          this.setState({
+            message: "foo@bar.baz forbidden error",
+          });
+        } else if (this.state.email === "") {
+          this.setState({
+            ...this.state,
+            message: "Must be a valid email",
+          });
+        }
+      });
+  };
 
   render() {
-    const { className } = this.props
+    const { className } = this.props;
     return (
       <div id="wrapper" className={className}>
         <div className="info">
@@ -82,7 +88,9 @@ onSubmit = event => {
           <div className="square" id="2"></div>
           <div className="square" id="3"></div>
           <div className="square" id="4"></div>
-          <div className="square active" id="5">B</div>
+          <div className="square active" id="5">
+            B
+          </div>
           <div className="square" id="6"></div>
           <div className="square" id="7"></div>
           <div className="square" id="8"></div>
@@ -92,17 +100,32 @@ onSubmit = event => {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left" onClick={this.leftClick}>LEFT</button>
-          <button id="up" onClick={this.upClick}>UP</button>
-          <button id="right" onClick={this.rightClick}>RIGHT</button>
-          <button id="down" onClick={this.downClick}>DOWN</button>
-          <button id="reset" onClick={this.resetClick}>reset</button>
+          <button id="left" onClick={this.leftClick}>
+            LEFT
+          </button>
+          <button id="up" onClick={this.upClick}>
+            UP
+          </button>
+          <button id="right" onClick={this.rightClick}>
+            RIGHT
+          </button>
+          <button id="down" onClick={this.downClick}>
+            DOWN
+          </button>
+          <button id="reset" onClick={this.resetClick}>
+            reset
+          </button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email" onChange={this.onChange}></input>
+          <input
+            id="email"
+            type="email"
+            placeholder="type email"
+            onChange={this.onChange}
+          ></input>
           <input id="submit" type="submit" onSubmit={this.onSubmit}></input>
         </form>
       </div>
-    )
+    );
   }
 }
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,82 +1,123 @@
-import React, {useState} from 'react'; 
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const initialState = {
-  x: 2, 
-  y: 2, 
-  steps: 0, 
-  email: '', 
-  message: ''
-}
-
+  x: 2,
+  y: 2,
+  steps: 0,
+  email: "",
+  message: "",
+};
 
 export default function AppFunctional(props) {
-  const [value, setValue] = useState(initialState)
+  const [value, setValue] = useState(initialState);
 
   const leftClick = () => {
-    console.log("left click")
-  }
+    value.x <= 3 && value.x >= 2
+      ? setValue({
+          ...value,
+          x: value.x - 1,
+          steps: value.steps + 1,
+        })
+      : setValue({
+          ...value,
+          x: value.x === 3 ? 3 : value.x,
+          message: "You can't go left",
+        });
+    console.log("left click");
+  };
 
   const upClick = () => {
-    console.log("up click")
-  }
+    value.y <= 3 && value.y >= 2
+      ? setValue({
+          ...value,
+          y: value.y - 1,
+          steps: value.steps + 1,
+        })
+      : setValue({
+          ...value,
+          y: value.y === 3 ? 3 : value.y,
+          message: "You can't go up",
+        });
+    console.log("up click");
+  };
 
   const rightClick = () => {
-    console.log("right click")
-  }
+    console.log("right click");
+  };
 
   const downClick = () => {
-    console.log("down click")
-  }
+    console.log("down click");
+  };
 
   const resetClick = () => {
-    console.log("reset click")
-  }
+    console.log("reset click");
+  };
 
-  const onChange = e => {
-    setValue({ 
-      ...value, 
-      email: e.target.value
-    })
-  }
+  const onChange = (e) => {
+    setValue({
+      ...value,
+      email: e.target.value,
+    });
+  };
 
-  const onSubmit = e => {
-    e.preventDefault(); 
-    
-    axios.post('http://localhost:9000/api/result', value)
-    .then(resp => {
-      setValue({ 
-        ...value, 
-        message: resp.data.message
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:9000/api/result", value)
+      .then((resp) => {
+        setValue({
+          ...value,
+          message: resp.data.message,
+        });
       })
-    })
-    .catch(error => {
-      console.log(error)
-      if (value.email === 'foo@bar.baz') {
-        setValue ({ 
-          ...value, 
-          message: "foo@bar.baz failure"
-        })
-      } else if (value.email === '') {
-        setValue ({ 
-          ...value, 
-          message: 'Email required'
-        })
-      } 
-    })
-  }
-  
+      .catch((error) => {
+        console.log(error);
+        if (value.email === "foo@bar.baz") {
+          setValue({
+            ...value,
+            message: "foo@bar.baz failure",
+          });
+        } else if (value.email === "") {
+          setValue({
+            ...value,
+            message: "Email required",
+          });
+        }
+      });
+  };
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">
+          Coordinates ({value.x}, {value.y})
+        </h3>
+        <h3 id="steps">You moved {value.steps} times</h3>
       </div>
       <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        <div
+          className={
+            value.x === 1 && value.y === 1 ? "square active" : "square"
+          }
+        >
+          {value.x === 1 && value.y === 1 ? "B" : ""}
+        </div>
+        <div
+          className={
+            value.x === 2 && value.y === 1 ? "square active" : "square"
+          }
+        >
+          {value.x === 2 && value.y === 1 ? "B" : ""}
+        </div>
+        <div
+          className={
+            value.x === 3 && value.y === 1 ? "square active" : "square"
+          }
+        >
+          {value.x === 3 && value.y === 1 ? "B" : ""}
+        </div>
         <div className="square"></div>
         <div className="square active">B</div>
         <div className="square"></div>
@@ -88,16 +129,31 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left" onClick={leftClick}>LEFT</button>
-        <button id="up" onClick={upClick}>UP</button>
-        <button id="right" onClick={rightClick}>RIGHT</button>
-        <button id="down" onClick={downClick}>DOWN</button>
-        <button id="reset" onClick={resetClick}>reset</button>
+        <button id="left" onClick={leftClick}>
+          LEFT
+        </button>
+        <button id="up" onClick={upClick}>
+          UP
+        </button>
+        <button id="right" onClick={rightClick}>
+          RIGHT
+        </button>
+        <button id="down" onClick={downClick}>
+          DOWN
+        </button>
+        <button id="reset" onClick={resetClick}>
+          reset
+        </button>
       </div>
       <form>
-        <input id="email" type="email" placeholder="type email" onChange={onChange}></input>
+        <input
+          id="email"
+          type="email"
+          placeholder="type email"
+          onChange={onChange}
+        ></input>
         <input id="submit" type="submit" onSubmit={onSubmit}></input>
       </form>
     </div>
-  )
+  );
 }
